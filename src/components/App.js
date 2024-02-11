a922696a9f6d99885313c45fc0fce8a0841f5e5e
Hayeong Pyeon <hayeong.pyeon@gmail.com>
2024-02-10 22:12:25 -0800

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import Books from './Books';
+
+export default function App() {
+  return (
+    <React.Fragment>
+      <Books />
+    </React.Fragment>
+  );
+}
---
+++ src/components/Books.js
@@ -0,0 +1,7 @@
+import React, { useState } from "react";
+
+export default function Books() {
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [books, setBooks] = useState([]);
+}
